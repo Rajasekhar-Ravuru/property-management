151832c245c8987ddef72b17830529e4925e921b
Rajasekhar-Ravuru <rajasekharravuru20@gmail.com>
2025-06-23 14:55:09 +0530

    added Field validations

--- src/main/java/com/mycompany/property/management/controller/UserController.java
+++ src/main/java/com/mycompany/property/management/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mycompany.property.management.controller;
 
 import com.mycompany.property.management.dto.UserDTO;
 import com.mycompany.property.management.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO){
         userDTO = userService.register(userDTO);
         ResponseEntity<UserDTO> responseEntity = new ResponseEntity<>(userDTO, HttpStatus.CREATED);
 
@@ -27,7 +28,7 @@ public class UserController {
     }
 
    @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> login(@Valid @RequestBody UserDTO userDTO){
         userDTO = userService.login(userDTO.getOwnerEmail(), userDTO.getPassword());
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
    }
--- src/main/java/com/mycompany/property/management/dto/UserDTO.java
+++ src/main/java/com/mycompany/property/management/dto/UserDTO.java
@@ -3,6 +3,9 @@ package com.mycompany.property.management.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +18,12 @@ public class UserDTO {
 
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner email is manidatory")
+    @NotEmpty(message = "Owner email cannot be empty")
+    @Size(min = 1, max = 50, message = "owner email ahould be between 1 to 50 characters in length  ")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password Cannot be null")
+    @NotEmpty(message = "Password Cannot be empty")
     private String password;
 }
